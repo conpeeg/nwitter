bf85ee800b5bb7548f11db786597181a8a147438
conpeeg <conpeeg@naver.com>
2022-05-24 17:42:34 +0900

    p129 user id add

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-import { useEffect, useState } from 'react';
-import AppRouter from 'components/Router';
-import {authService} from 'fbase';
+import React, { useState, useEffect } from "react";
+import AppRouter from "components/Router";
+import { authService } from "fbase";
 
 function App() {
   const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
         setIsLoggedIn(true);
+        setUserObj(user);
       } else {
         setIsLoggedIn(false);
       }
-      setInit(true);  
-    });  
-    }, []);
-
+      setInit(true);
+    });
+  }, []);
   return (
     <>
       {init ? (
-      <AppRouter isLoggedIn={isLoggedIn} />
+        <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />
       ) : (
-        "initializing..."
+        "Initializing..."
       )}
-      <footer> &copy; {new Date().getFullYear()} Nwitter Conpeeg </footer>
-    </>    
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+    </>
   );
 }
 
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,41 +1,33 @@
-import { useState } from 'react';
-import {
-    BrowserRouter as Router,
-    Route,    
-    Switch,
-    
-} from "react-router-dom";
-import Auth from 'routes/Auth';
-import Home from 'routes/Home';
+import React from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
 
-import Profile from 'routes/Profile';
-import Navigation from './Navigation';
-
-const AppRouter = ({isLoggedIn}) => {
-
-    return (
-        <Router>
-            {isLoggedIn && <Navigation />}
-            
-            <Switch>
-                {isLoggedIn ? (
-                    <>
-                    <Route exact path="/" component={Home} />
-                        
-                    <Route path="/profile" component={Profile} />
-                        
-                    </>
-                ) : (
-                    <>
-                    <Route exact path="/">
-                        <Auth />
-                    </Route>
-                    </>
-                )}
-                
-            </Switch>
-        </Router>
-    )
-}
-
+const AppRouter = ({ isLoggedIn, userObj }) => {
+  return (
+    <Router>
+      {isLoggedIn && <Navigation />}
+      <Switch>
+        {isLoggedIn ? (
+          <>
+            <Route exact path="/">
+              <Home userObj={userObj} />
+            </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+          </>
+        ) : (
+          <>
+            <Route exact path="/">
+              <Auth />
+            </Route>
+          </>
+        )}
+      </Switch>
+    </Router>
+  );
+};
 export default AppRouter;
